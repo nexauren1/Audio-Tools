874e5503b60db8225aa45301c77008d68e589a3c
nexauren1 <nexaurenstore@gmail.com>
2026-09-18 23:07:24 +0200

    Fix built-in Kotlin JVM configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
     buildFeatures { buildConfig = true }
 }
 dependencies {
