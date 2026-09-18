eadfd1df7c4756a3a59cd25a9e8a6021a9c7dbcf
nexauren1 <nexaurenstore@gmail.com>
2026-09-18 23:01:57 +0200

    Add notification and background update foundation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,5 +35,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.18.0")
     implementation("androidx.activity:activity-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.8.0")
+    implementation("androidx.work:work-runtime-ktx:2.11.1")
     implementation("com.google.android.material:material:1.14.0")
 }
