80a9d39c60299492d99825e5e2172c36462be1a6
nexauren1 <nexaurenstore@gmail.com>
2026-09-19 04:24:33 +0200

    release: bump Audio Tools to v0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.nexauren.audiotools"
         minSdk = 23
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
