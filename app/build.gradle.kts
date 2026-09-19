cb923dc8f4e1737b9a258be247b47b8cdfe974c5
nexauren1 <nexaurenstore@gmail.com>
2026-09-19 05:19:26 +0200

    chore: bump Audio Tools to 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.nexauren.audiotools"
         minSdk = 23
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
